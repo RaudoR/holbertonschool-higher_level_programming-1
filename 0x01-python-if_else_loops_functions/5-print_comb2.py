d3002fd3f8acef9e826ef7ada1f292ed53446b3a
Kevin Yook <yook00627@gmail.com>
2018-04-10 17:49:35 +0000

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-for n in range(0,100):
-    if n < 10:
-        print(format(n, '02d'), end = '')
-    else:
-        print(n, end = '')
+for n in range(0, 100):
     if n in range(0, 99):
-        print(", ", end = '')
+        print("{}, ".format(format(n, '02d')), end = '')
     else:
-        print()
+        print(n)
