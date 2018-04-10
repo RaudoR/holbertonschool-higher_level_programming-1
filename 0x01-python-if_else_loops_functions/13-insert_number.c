98dcadfe36aaac0f4cd7f965ee6a06e5ec250762
Kevin Yook <yook00627@gmail.com>
2018-04-10 23:11:10 +0000

    insert node if its empty

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,12 +13,16 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *holder, *new;
 
-	if (*head == NULL)
-		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 	holder = *head;
 	if (holder->n > number)
 	{
@@ -29,20 +33,14 @@ listint_t *insert_node(listint_t **head, int number)
 	else
 		while (holder->next != NULL)
 		{
-			if (holder->n < number && holder->next->n > number)
+			if (holder->n <= number && holder->next->n >= number)
 			{
 				new->next = holder->next;
 				holder->next = new;
 				return (new);
 			}
-			if (holder->n == number)
-			{
-				free(new);
-				return (NULL);
-			}
 			holder = holder->next;
 		}
-	new->next = NULL;
 	holder->next = new;
 	return (new);
 }
