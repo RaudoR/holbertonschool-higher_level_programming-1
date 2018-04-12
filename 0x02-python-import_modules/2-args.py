45b8d5103cd2ae61ac790a4bb366198abc88d602
Kevin Yook <yook00627@gmail.com>
2018-04-12 19:43:22 +0000

    change if to elif

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     length = len(argv)
     if length == 1:
         print("{} {}.".format(length - 1, "arguments"))
-    if length == 2:
+    elif length == 2:
         print("{} {}:".format(length - 1, "argument"))
         print("{}: {}".format(length - 1, argv[1]))
     else:
