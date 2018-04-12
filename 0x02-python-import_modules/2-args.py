6a66c9f7a397b4197fe7aa117b88b04014b46642
Kevin Yook <yook00627@gmail.com>
2018-04-12 19:40:53 +0000

    print arugment instead of arguments if arg is 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     length = len(argv)
     if length == 1:
         print("{} {}.".format(length - 1, "arguments"))
+    if length == 2:
+        print("{} {}:".format(length - 1, "argument"))
+        print("{}: {}".format(length - 1, argv[1]))
     else:
         for i in range(length):
             if i == 0:
