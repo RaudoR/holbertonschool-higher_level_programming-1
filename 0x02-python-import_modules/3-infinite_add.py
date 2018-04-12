6ee280eac7ba78890f7a3b15318866231f72cf44
Kevin Yook <yook00627@gmail.com>
2018-04-12 17:41:24 +0000

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    num = 0
+    length = len(argv)
+    for i in range(1, length):
+        num += int(argv[i])
+    print(num)
