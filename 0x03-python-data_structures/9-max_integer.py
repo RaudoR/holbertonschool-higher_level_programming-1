8ccfbef4afa252fd43737a8d119dd2b9312cbf78
Kevin Yook <yook00627@gmail.com>
2018-04-17 19:44:53 +0000

    check if list is null in a different way

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    length = len(my_list)
-    if length == 0:
-        return
-    num = 0
-    for i in range(length):
-        if my_list[i] > num:
-            num = my_list[i]
-    return num
+    if my_list:
+        num = 0
+        for i in my_list:
+            if i > num:
+                num = i
+        return num
+    return
