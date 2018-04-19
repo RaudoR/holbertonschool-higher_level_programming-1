6b7ec3a275338379c1dbfd9f395f2d5637af06fa
Kevin Yook <yook00627@gmail.com>
2018-04-19 19:40:13 +0000

    no empty check

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        new_list = [replace if i == search else i for i in my_list]
-        return new_list
+    new_list = [replace if i == search else i for i in my_list]
+    return new_list
