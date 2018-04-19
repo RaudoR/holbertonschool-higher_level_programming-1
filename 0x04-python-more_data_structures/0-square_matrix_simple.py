d71f9f08f5f881ceae9f74d6b6dc9427b6a8e7d8
Kevin Yook <yook00627@gmail.com>
2018-04-19 17:13:51 +0000

    squared matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def squared(x):
+    return x**2
+def square_matrix_simple(matrix=[]):
+    new_m = []
+    for l in matrix:
+        new_m += [list(map(squared, l))]
+    return new_m
