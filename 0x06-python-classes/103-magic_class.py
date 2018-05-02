8f55144782d558caa04e70803a6fda1d41b3c200
Kevin Yook <yook00627@gmail.com>
2018-05-02 00:06:04 +0000

    added new lines

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+
+
+
+
+
 class MagicClass:
     def __init__(self, radius):
         self._MagicClass__radius = 0
@@ -6,8 +12,10 @@ class MagicClass:
             raise TypeError('radius must be a number')
         self._MagicClass__radius = radius
 
+
     def area(self):
         return self._MagicClass__radius ** 2 * math.pi
+
 
     def circumference(self):
         return 2 * match.pi * self._MagicClass__radius
