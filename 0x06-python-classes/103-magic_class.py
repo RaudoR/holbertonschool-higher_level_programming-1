a1a0ed6d98d8f6ccb5a00397ab678d81e7af1813
Kevin Yook <yook00627@gmail.com>
2018-05-02 03:40:03 +0000

    got rid of magic class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,15 +7,15 @@ class MagicClass:
         """init
 
         init method"""
-        self._MagicClass__radius = 0
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self._MagicClass__radius = radius
+        self.__radius = radius
 
     def area(self):
         """area"""
-        return self._MagicClass__radius ** 2 * math.pi
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """circumferencec"""
-        return 2 * match.pi * self._MagicClass__radius
+        return 2 * match.pi * self.__radius
