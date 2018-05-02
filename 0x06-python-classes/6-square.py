4fe84da715a171b532c2af46d3bb41e15279f9d8
Kevin Yook <yook00627@gmail.com>
2018-05-02 00:31:03 +0000

    change for and statement

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,11 +10,11 @@ class Square:
         __size (int): size of square
     """
     def __init__(self, size=0, position=(0, 0)):
-        if type(value) != int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        if type(value) != tuple:
+        if type(position) != tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.size = size
@@ -63,7 +63,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(value[0]) != int and type(value[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[1] < 0 and value[0] < 0:
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
