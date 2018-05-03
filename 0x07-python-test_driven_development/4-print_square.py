24b28c06e206a55fcd8dd55d7101686e24b60c56
Kevin Yook <yook00627@gmail.com>
2018-05-03 23:27:01 +0000

    added docstrings

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+This module prints square
+"""
+
+
 def print_square(size):
+    """
+    prints square of given size
+    Args:
+        size: input integer
+    Raises:
+        TypeError: if size is not int
+        ValueError: if size is less than 0
+    """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
