3d63881e32ad408bbda2822cb2ce8e3538b8890c
Kevin Yook <yook00627@gmail.com>
2018-05-04 04:51:40 +0000

    pep8

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,6 +4,7 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     def test_docstring(self):
         doc = __import__('6-max_integer').__doc__
