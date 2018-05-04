9b09753f26f6b337546e9752442fd4bba77de298
Kevin Yook <yook00627@gmail.com>
2018-05-04 05:28:08 +0000

    docstring added

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+This module prints text with indentation
+"""
+
+
 def text_indentation(text):
+    """
+    prints indentation when there is a . ? or :
+    Args:
+        text: input string
+    Raises:
+        TypeError: check if text is a string
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
     text = text.replace('.', '.\n')
