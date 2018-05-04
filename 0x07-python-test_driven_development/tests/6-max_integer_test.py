c0fd8837e030a35b76bae35ca63c6a19a1e0bc80
Kevin Yook <yook00627@gmail.com>
2018-05-04 04:40:24 +0000

    test 5 with unittest:

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_docstring(self):
+        doc = __import__('6-max_integer').__doc__
+        self.assertIsNotNone(doc)
+        self.assertIsNotNone(max_integer.__doc__)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_positive(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+
+    def test_one_item(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_negative(self):
+        self.assertEqual(max_integer([-1, -2, -1, -4, -5]), -1)
+
+    def test_float(self):
+        self.assertEqual(max_integer([2.5, 3.6, 2.9, 3.9]), 3.9)
+
+    def test_character(self):
+        self.assertEqual(max_integer(['a', 'b', 'c']), 'c')
