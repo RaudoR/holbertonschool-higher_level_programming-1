65bdd1b0d48f24bc8928e0f5643e2dc3032825fe
Kevin Yook <yook00627@gmail.com>
2018-05-05 00:08:56 +0000

    new line chagne

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,7 +14,7 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    text = text.replace('.', '.\n')
-    text = text.replace('?', '?\n')
-    text = text.replace(':', ':\n')
-    print("\n\n".join(list(l.strip() for l in text.split('\n'))), end="")
+    text = text.replace('.', '.\n\n')
+    text = text.replace('?', '?\n\n')
+    text = text.replace(':', ':\n\n')
+    print("\n".join(list(l.strip() for l in text.split('\n'))), end="")
