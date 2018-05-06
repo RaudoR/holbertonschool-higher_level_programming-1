942d600e1f27322484ff652f4132de5eb8209275
Kevin Yook <yook00627@gmail.com>
2018-05-06 01:12:08 +0000

    got rid of edge case and only did numpy multiply

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,7 +2,7 @@
 """
 This module multiply 2 matricies using numpy module
 """
-import numpy as np
+import numpy
 
 
 def lazy_matrix_mul(m_a, m_b):
@@ -11,34 +11,7 @@ def lazy_matrix_mul(m_a, m_b):
     Args:
         m_a: input first matrix
         m_b: input second matrix
-    Raises:
-        TypeError: if m_a or m_b is not list
-        ValueError: if m_a or m_b is None
-        TypeError: if both matricies don't have an int or float
-        TypeError: if both matricies are not rectagles or squares
     Returns:
         return m_a * m_b
     """
-    if type(m_a) is not list or\
-       not all(isinstance(l_a, list) for l_a in m_a):
-        raise TypeError("m_a must be a list")
-    if type(m_b) is not list or\
-       not all(isinstance(l_b, list) for l_b in m_b):
-        raise TypeError("m_b must be a list")
-    if m_a == [] or not all(l_a != [] for l_a in m_a):
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or not all(l_b != [] for l_b in m_b):
-        raise ValueError("m_b can't be empty")
-    if not all(isinstance(i_a, (int, float)) for l_a in m_a for i_a in l_a):
-        raise TypeError("m_a should contain only integers or floats")
-    if not all(isinstance(i_b, (int, float)) for l_b in m_b for i_b in l_b):
-        raise TypeError("m_b should contain only integers or floats")
-    if not all(len(l_a) is len(m_a[0]) for l_a in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
-    if not all(len(l_b) is len(m_b[0]) for l_b in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-    ma = np.matrix(m_a)
-    mb = np.matrix(m_b)
-    return ma * mb
+    return numpy.matmul(m_a, m_b)
