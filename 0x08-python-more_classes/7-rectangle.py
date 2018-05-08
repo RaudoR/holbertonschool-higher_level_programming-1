93864a437e00f68cd0079f3a20a5df0b64e67afc
Kevin Yook <yook00627@gmail.com>
2018-05-08 16:28:12 +0000

    change error

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -61,9 +61,9 @@ class Rectangle:
             ValueError: value is less than 0
         """
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
@@ -90,4 +90,4 @@ class Rectangle:
     def __del__(self):
         """delete the square inputed"""
         Rectangle.number_of_instances -= 1
-        print("Bye rectangle…")
+        print("Bye rectangle...")
