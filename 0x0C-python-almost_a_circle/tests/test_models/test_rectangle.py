896eebc9d4102daf1cb8cde270cb1fbd9bc47042
Kevin Yook <yook00627@gmail.com>
2018-05-19 21:06:25 +0000

    added more test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -93,77 +93,95 @@ class TestRectangle(unittest.TestCase):
         """check display square with #"""
         holder = io.StringIO()
         sys.stdout = holder
-        r1 = Rectangle(2, 2)
-        r1.display()
+        t1 = Rectangle(3, 2)
+        t1.display()
         sys.stdout = sys.__stdout__
-        self.assertEqual(holder.getvalue(), "##\n##\n")
+        self.assertEqual(holder.getvalue(), "###\n###\n")
 
     def test_str_return(self):
         """check if the str returns right"""
-        r1 = Rectangle(4, 6, 2, 1, 12)
-        self.assertEqual(r1.__str__(), "[Rectangle] (12) 2/1 - 4/6")
+        t1 = Rectangle(4, 6, 2, 1, 12)
+        self.assertEqual(t1.__str__(), "[Rectangle] (12) 2/1 - 4/6")
 
     def test_display_with_xy(self):
         """check if display with cordinates work"""
         holder = io.StringIO()
         sys.stdout = holder
-        r1 = Rectangle(2, 2, 2, 2, 10)
-        r1.display()
+        t1 = Rectangle(3, 2, 2, 2, 10)
+        t1.display()
         sys.stdout = sys.__stdout__
-        self.assertEqual(holder.getvalue(), "\n\n  ##\n  ##\n")
+        self.assertEqual(holder.getvalue(), "\n\n  ###\n  ###\n")
 
     def test_update(self):
         """check if update works right"""
-        r1 = Rectangle(10, 10, 10, 10)
-        r1.update(89)
-        self.assertEqual(r1.__str__(), "[Rectangle] (89) 10/10 - 10/10")
-        r1.update(89, 2)
-        self.assertEqual(r1.__str__(), "[Rectangle] (89) 10/10 - 2/10")
-        r1.update(89, 2, 3)
-        self.assertEqual(r1.__str__(), "[Rectangle] (89) 10/10 - 2/3")
-        r1.update(89, 2, 3, 4)
-        self.assertEqual(r1.__str__(), "[Rectangle] (89) 4/10 - 2/3")
-        r1.update(89, 2, 3, 4, 5)
-        self.assertEqual(r1.__str__(), "[Rectangle] (89) 4/5 - 2/3")
+        t1 = Rectangle(10, 10, 10, 10)
+        t1.update(89)
+        self.assertEqual(t1.__str__(), "[Rectangle] (89) 10/10 - 10/10")
+        t1.update(89, 2)
+        self.assertEqual(t1.__str__(), "[Rectangle] (89) 10/10 - 2/10")
+        t1.update(89, 2, 3)
+        self.assertEqual(t1.__str__(), "[Rectangle] (89) 10/10 - 2/3")
+        t1.update(89, 2, 3, 4)
+        self.assertEqual(t1.__str__(), "[Rectangle] (89) 4/10 - 2/3")
+        t1.update(89, 2, 3, 4, 5)
+        self.assertEqual(t1.__str__(), "[Rectangle] (89) 4/5 - 2/3")
 
     def test_update2(self):
         """check if update works with morearguments"""
-        r1 = Rectangle(10, 10, 10, 10)
-        r1.update(89, 2, 3, 4, 5, 10)
-        self.assertEqual(r1.__str__(), "[Rectangle] (89) 4/5 - 2/3")
+        t1 = Rectangle(10, 10, 10, 10)
+        t1.update(89, 2, 3, 4, 5, 10)
+        self.assertEqual(t1.__str__(), "[Rectangle] (89) 4/5 - 2/3")
 
     def test_update_width_check(self):
         """check if update checks width validation"""
-        r1 = Rectangle(10, 10, 10, 10)
+        t1 = Rectangle(10, 10, 10, 10)
         with self.assertRaises(TypeError):
-            r1.update(89, "Yo", 3, 4, 5)
+            t1.update(89, "Yo", 3, 4, 5)
         with self.assertRaises(ValueError):
-            r1.update(89, -5, 3, 4, 5)
+            t1.update(89, -5, 3, 4, 5)
         with self.assertRaises(ValueError):
-            r1.update(89, 0, 3, 4, 5)
+            t1.update(89, 0, 3, 4, 5)
 
     def test_update_height_check(self):
         """check if update checks height validation"""
-        r1 = Rectangle(10, 10, 10, 10)
+        t1 = Rectangle(10, 10, 10, 10)
         with self.assertRaises(TypeError):
-            r1.update(89, 3, "Yo", 4, 5)
+            t1.update(89, 3, "Yo", 4, 5)
         with self.assertRaises(ValueError):
-            r1.update(89, 5, -3, 4, 5)
+            t1.update(89, 5, -3, 4, 5)
         with self.assertRaises(ValueError):
-            r1.update(89, 3, 0, 4, 5)
+            t1.update(89, 3, 0, 4, 5)
 
     def test_update_x_check(self):
         """check if update checks x validation"""
-        r1 = Rectangle(10, 10, 10, 10)
+        t1 = Rectangle(10, 10, 10, 10)
         with self.assertRaises(TypeError):
-            r1.update(89, 3, 5, "Yo", 5)
+            t1.update(89, 3, 5, "Yo", 5)
         with self.assertRaises(ValueError):
-            r1.update(89, 5, 5, -4, 5)
+            t1.update(89, 5, 5, -4, 5)
 
     def test_update_y_check(self):
         """check if update checks y validation"""
-        r1 = Rectangle(10, 10, 10, 10)
+        t1 = Rectangle(10, 10, 10, 10)
         with self.assertRaises(TypeError):
-            r1.update(89, 3, 4, 4, "yo")
+            t1.update(89, 3, 4, 4, "yo")
         with self.assertRaises(ValueError):
-            r1.update(89, 5, 3, 4, -5)
+            t1.update(89, 5, 3, 4, -5)
+
+    def test_update_kwargs_check(self):
+        """check if kwars update works"""
+        t1 = Rectangle(10, 10, 10, 10, 10)
+        t1.update(height=1)
+        self.assertEqual(t1.__str__(), "[Rectangle] (10) 10/10 - 10/1")
+        t1.update(width=1, x=2)
+        self.assertEqual(t1.__str__(), "[Rectangle] (10) 2/10 - 1/1")
+        t1.update(y=1, width=2, x=3, id=89)
+        self.assertEqual(t1.__str__(), "[Rectangle] (89) 3/1 - 2/1")
+        t1.update(x=1, height=2, y=3, width=4)
+        self.assertEqual(t1.__str__(), "[Rectangle] (89) 1/3 - 4/2")
+
+    def test_update_kwargs_with_args(self):
+        """check if args exist it ignores kwargs"""
+        t1 = Rectangle(10, 10, 10, 10, 10)
+        t1.update(24, 3, height=1)
+        self.assertEqual(t1.__str__(), "[Rectangle] (24) 10/10 - 3/10")
