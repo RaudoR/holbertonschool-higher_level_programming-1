86abe2cf107689cad6ba1ce2407641d3abb77e92
Kevin Yook <yook00627@gmail.com>
2018-06-25 13:08:38 -0700

    fixed some syntax

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- This updates Bob's scores for the table in the database
-UPDATE 'second_table' SET 'score' = 10 WHERE 'second_table'.'name' = 'Bob';
+UPDATE `second_table` SET `score` = 10 WHERE `second_table`.`name` = 'Bob';
