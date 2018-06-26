877035e9eae6d4883e6eb17d18030d616fb4a89a
Kevin Yook <yook00627@gmail.com>
2018-06-26 12:11:30 -0700

    added use

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,7 @@
 -- state_id INT can't be null and inherit from states table,
 -- name VARCHAR(256) not null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id),
