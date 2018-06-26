b48e970307f2f744f0cfe1ffea83a17c7b067c77
Kevin Yook <yook00627@gmail.com>
2018-06-26 14:22:21 -0700

    change all to inner

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -9,7 +9,7 @@ WHERE tv_genres.name NOT IN (SELECT tv_genres.name
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-left OUTER JOIN tv_genres
+INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter')
 ORDER BY tv_genres.name ASC
