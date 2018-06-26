8f098d630f846220b86241e4bb745480c6f312ae
Kevin Yook <yook00627@gmail.com>
2018-06-26 14:15:27 -0700

    chagne to inner

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,9 @@
 -- This list genres that are not in Dexter
 SELECT DISTINCT tv_genres.name
 FROM tv_shows
-LEFT OUTER JOIN tv_show_genres
+INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-LEFT OUTER JOIN tv_genres
+INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name NOT IN (SELECT tv_genres.name
 FROM tv_shows
