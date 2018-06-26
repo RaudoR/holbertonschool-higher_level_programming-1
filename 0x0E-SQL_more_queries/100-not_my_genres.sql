dfcf6c045351e1eb3b16255c9260347452c1960b
Kevin Yook <yook00627@gmail.com>
2018-06-26 11:24:52 -0700

    change some join

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,9 @@
 -- This list all tv shows with genres
 SELECT DISTINCT tv_genres.name
 FROM tv_shows
-INNER JOIN tv_show_genres
+LEFT OUTER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres
+LEFT OUTER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name NOT IN (SELECT tv_genres.name
 FROM tv_shows
