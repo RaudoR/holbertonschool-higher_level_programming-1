d4c21fa36de0b29bc0e993e7bfe3ae2bfdc00b68
Kevin Yook <yook00627@gmail.com>
2018-06-26 19:31:41 -0700

    delete redundent code

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,10 +1,6 @@
 -- This list genres that are not in Dexter
 SELECT DISTINCT tv_genres.name
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
+FROM tv_genres
 WHERE tv_genres.name NOT IN (SELECT tv_genres.name
 FROM tv_shows
 INNER JOIN tv_show_genres
