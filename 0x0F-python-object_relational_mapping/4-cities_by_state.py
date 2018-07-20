3d191a46a450b0287a05f10838df5ce3eec99dc6
Kevin Yook <yook00627@gmail.com>
2018-07-19 19:02:48 -0700

    joining using state table with city table using state id

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values in the states table
+where name matches the argument safe from SQL injection
+Args:
+    mysql username: name of the user
+    mysql password: password for database
+    database name: name of the database
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    INNER JOIN states ON cities.state_id = states.id\
+    ORDER BY cities.id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
