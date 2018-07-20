6e524525b7ca2fbac3a25a8cf0fa68903d630d3e
Kevin Yook <yook00627@gmail.com>
2018-07-19 20:35:58 -0700

    get all the states using sql alchemy and also get the first element of the table

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""lists all State objects from the database
+Args:
+    mysql username: name of the user
+    mysql password: password for database
+    database name: name of the database
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker()
+    session = Session(bind=engine)
+    Base.metadata.create_all(engine)
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
