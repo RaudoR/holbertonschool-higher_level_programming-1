968df02f92613cfc6a51a508703b0aece35f480d
Kevin Yook <yook00627@gmail.com>
2018-07-19 18:29:34 -0700

    added searching for states that starts with N and also finds states that matches name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values in the states table
+where name matches the argument
+Args:
+    mysql username: name of the user
+    mysql password: password for database
+    database name: name of the database
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+    WHERE name = '{}'\
+    ORDER BY states.id ASC".format(argv[4]))
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
