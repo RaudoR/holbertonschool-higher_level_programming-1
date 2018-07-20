a3c03076dd10a3a9e0e249870d9c5c2fb30bf8f2
Kevin Yook <yook00627@gmail.com>
2018-07-20 16:32:02 -0700

    get rid of typo

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     for state in session.query(State).order_by(State.id).all():
         print("{:d}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("   sxc {:d}: {}".format(city.id, city.name))
+            print("    {:d}: {}".format(city.id, city.name))
     session.close()
