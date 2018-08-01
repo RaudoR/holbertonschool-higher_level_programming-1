890af7f0311042ad57438eba9c66892b2aad95e1
Kevin Yook <yook00627@gmail.com>
2018-07-31 18:58:04 -0700

    finding the peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""find peak in a list of unsorted integers"""
+
+
+def peak_finder(int_list, start, fin):
+    """this usese recurrsion to find peak"""
+    if start == fin:
+        return int_list[start]
+    mid = (fin + start) // 2
+    if int_list[mid] < int_list[mid + 1]:
+        return peak_finder(int_list, mid + 1, fin)
+    return peak_finder(int_list, start, mid)
+
+
+def find_peak(list_of_integers):
+    """this function find the peak"""
+    if not list_of_integers:
+        return
+    return peak_finder(list_of_integers, 0, len(list_of_integers) - 1)
