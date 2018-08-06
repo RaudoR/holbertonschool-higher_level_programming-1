090bb563baa43937566f128d51848d67b8aa290c
Kevin Yook <yook00627@gmail.com>
2018-08-06 16:28:13 -0700

    getting the films asscoiated with the search names

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""takes in a string and sends a search request to the Star Wars API"""
+from requests import get
+from sys import argv
+if __name__ == "__main__":
+    page = 1
+    json = get("https://swapi.co/api/people/?search={}&page={}".
+               format(argv[1], page)).json()
+    print("Number of results: {}".format(json.get("count")))
+    while json.get("count"):
+        for arg in json.get("results"):
+            print(arg.get("name"))
+            for film in arg.get("films"):
+                print("\t{}".format(get(film).json().get("title")))
+        page += 1
+        json = get("https://swapi.co/api/people/?search={}&page={}".
+                   format(argv[1], page)).json()
