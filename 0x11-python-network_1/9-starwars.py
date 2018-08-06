6bcefb0c020ae5735f0ca7e91cbef03b22a29669
Kevin Yook <yook00627@gmail.com>
2018-08-06 12:19:24 -0700

    searching thorugh the starwars api

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""takes in a string and sends a search request to the Star Wars API"""
+from requests import get
+from sys import argv
+if __name__ == "__main__":
+    json = get("https://swapi.co/api/people/?search={}".
+                format(argv[1])).json()
+    print("Number of results: {}".format(json["count"]))
+    for args in json["results"]:
+        print(args["name"])
