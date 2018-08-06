f2cdde714c6009c2b0684dc61cbc76c714907808
Kevin Yook <yook00627@gmail.com>
2018-08-06 11:23:21 -0700

    change to use argv instead

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
 import requests
+from sys import argv
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get(argv[1])
     print(r.headers.get("X-Request-Id"))
