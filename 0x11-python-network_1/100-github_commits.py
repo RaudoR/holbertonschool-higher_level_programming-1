d4c24ee4231141ae2543b0ab895c2ab5871b17c3
Kevin Yook <yook00627@gmail.com>
2018-08-06 13:07:47 -0700

    find the people who commited in the git hub

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""takes your Github credentials (username and password) and
+uses the Github API to display your id
+"""
+from requests import get
+from sys import argv
+if __name__ == "__main__":
+    json = get("https://api.github.com/repos/{}/{}/commits".
+               format(argv[1], argv[2])).json()
+    count = 0
+    for arg in json:
+        count += 1
+        name = arg.get("commit").get("author").get("name")
+        sha = arg.get("sha")
+        print("{}: {}".format(sha, name))
+        if count == 10:
+            break
