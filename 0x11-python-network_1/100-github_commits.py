a6de6ecdbceca5ed259b3c61492643cae661792c
Kevin Yook <yook00627@gmail.com>
2018-08-06 13:19:26 -0700

    add imports in the if check

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,9 +2,9 @@
 """takes your Github credentials (username and password) and
 uses the Github API to display your id
 """
-from requests import get
-from sys import argv
 if __name__ == "__main__":
+    from requests import get
+    from sys import argv
     json = get("https://api.github.com/repos/{}/{}/commits".
                format(argv[1], argv[2])).json()
     count = 0
