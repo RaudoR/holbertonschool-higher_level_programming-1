ec637b97df5a8674a8185aef30d238be92a2052f
Kevin Yook <yook00627@gmail.com>
2018-08-06 10:54:21 -0700

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the response
+"""
+from urllib import request, parse
+from sys import argv
+if __name__ == "__main__":
+    data = parse.urlencode({"email": argv[2]}).encode()
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as response:
+        print(response.read().decode("utf8"))
