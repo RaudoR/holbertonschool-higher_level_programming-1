42deb43ae844558c1f1a0ec0c641151aa6492a2a
Kevin Yook <yook00627@gmail.com>
2018-08-06 17:20:11 -0700

    added try except to catch errors

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -7,11 +7,14 @@ if __name__ == "__main__":
     json = get("https://swapi.co/api/people/?search={}&page={}".
                format(argv[1], page)).json()
     print("Number of results: {}".format(json.get("count")))
-    while json.get("count"):
-        for arg in json.get("results"):
-            print(arg.get("name"))
-            for film in arg.get("films"):
-                print("\t{}".format(get(film).json().get("title")))
-        page += 1
-        json = get("https://swapi.co/api/people/?search={}&page={}".
-                   format(argv[1], page)).json()
+    try:
+        while json.get("count"):
+            for arg in json.get("results"):
+                print(arg.get("name"))
+                for film in arg.get("films"):
+                    print("\t{}".format(get(film).json().get("title")))
+            page += 1
+            json = get("https://swapi.co/api/people/?search={}&page={}".
+                       format(argv[1], page)).json()
+    except Exception:
+        pass
