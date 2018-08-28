f75df86475f491fe5a4887831635ebef2bd64238
yook00627 <yook00627@gmail.com>
2018-08-28 10:25:56 -0700

    get factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial (a) {
+  if (isNaN(a)) {
+    return 1;
+  }
+  if (a === 1) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
+}
+console.log(factorial(parseInt(process.argv[2])));
