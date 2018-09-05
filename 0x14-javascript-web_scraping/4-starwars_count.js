f787c46390d99e6063dbd7096f03727fbb7e1ca0
yook00627 <yook00627@gmail.com>
2018-09-05 09:34:42 -0700

    fix semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,14 +4,12 @@ request.get(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else if (response.statusCode === 200) {
-    let c = 0;
+    let count = 0;
     for (let film of JSON.parse(body).results) {
-      for(let chr of film.characters) {
-	if (chr.search('/18/') > 0) {
-	  c++;
-	}
+      for (let chr of film.characters) {
+        if (chr.search('/18/') > 0) { count++; }
       }
     }
-    console.log(c);
+    console.log(count);
   }
 });
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,11 +7,7 @@ request.get(process.argv[2], function (error, response, body) {
     let completed = {};
     for (let task of JSON.parse(body)) {
       if (task.completed === true) {
-	if (task.userId in completed) {
-	  completed[task.userId]++;
-	} else {
-	  completed[task.userId] = 1;
-	}
+        if (task.userId in completed) { completed[task.userId]++; } else { completed[task.userId] = 1; }
       }
     }
     console.log(completed);
